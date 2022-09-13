9560d44792aeee2283b9f742c927cc31fe9b75ed
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-13 21:10:58 +0200

    redirecting login to home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import { BrowserRouter,Switch,Route  } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const {user, authIsReady} = useAuthContext();
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar/>
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route exact path="/login">
-          <Login/>
-        </Route>
-        <Route exact path="/register">
-          <Signup/>
-        </Route>
-      </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              {user &&<Home />}
+              {!user && <Redirect to="login"/>}
+            </Route>
+            <Route exact path="/login">
+             {!user && <Login /> }
+             {user && <Redirect to="/" />}
+            </Route>
+            <Route exact path="/register">
+              {!user && <Signup />}
+              {user && <Redirect to="/"/>}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
