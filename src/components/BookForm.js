981e680ce6f8fe90ed5c97e68df60ee95df88bad
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-13 21:16:44 +0200

    adding uid when adding new book

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,24 +1,32 @@
-import React,{useState} from 'react';
-import {db} from "../firebase/config"
-import { collection,addDoc } from 'firebase/firestore';
-const BookForm = () => {
-    const [newBook,setNewBook]=useState('')
+import React, { useState } from "react";
+import { db } from "../firebase/config";
+import { collection, addDoc} from "firebase/firestore";
+import { useAuthContext } from "../hooks/useAuthContext";
 
-    const handleSubmit=async (e)=>{
-    e.preventDefault()
+const BookForm = () => {
+  const [newBook, setNewBook] = useState("");
+  const { user } = useAuthContext();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     //console.log(newBook)
-    const ref=collection(db,'books')
-    await addDoc(ref,{title:newBook})
-    setNewBook('')
-}
-    return (
-        <form onSubmit={handleSubmit}>
-            <label><span>Add new book : </span>
-            <input type="text" required onChange={((e)=>setNewBook(e.target.value))} value={newBook}/>
-            </label>
-            <button>Add</button>
-        </form>
-    );
-}
+    const ref = collection(db, "books");
+    await addDoc(ref, { title: newBook, uid: user.uid });
+    setNewBook("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <span>Add new book : </span>
+        <input
+          type="text"
+          required
+          onChange={(e) => setNewBook(e.target.value)}
+          value={newBook}
+        />
+      </label>
+      <button>Add</button>
+    </form>
+  );
+};
 
 export default BookForm;
